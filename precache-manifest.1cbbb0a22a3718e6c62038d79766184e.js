0f10bdbf57be55a767880b0111f13cfbb815a340
Andrii <remote.makandriival@gmail.com>
2022-03-23 19:42:44 +0200

    Updates

---
+++ precache-manifest.1cbbb0a22a3718e6c62038d79766184e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "76c241d8d842c21091bd",
+    "url": "/makval/static/css/main.270e29dc.chunk.css"
+  },
+  {
+    "revision": "76c241d8d842c21091bd",
+    "url": "/makval/static/js/main.a0e02e9e.chunk.js"
+  },
+  {
+    "revision": "6cc708ec7acecdad7d0c",
+    "url": "/makval/static/js/runtime~main.4a39a675.js"
+  },
+  {
+    "revision": "9527a551113c0357b139",
+    "url": "/makval/static/js/2.1ea063e2.chunk.js"
+  },
+  {
+    "revision": "ea8394860e1284847fa91a0d69eccd57",
+    "url": "/makval/static/media/img-about.ea839486.png"
+  },
+  {
+    "revision": "d08c26edae9ed3b0d396098bcb9d258c",
+    "url": "/makval/static/media/tat.d08c26ed.png"
+  },
+  {
+    "revision": "bb456ca9dbcf97de8cb42630f6e0427d",
+    "url": "/makval/static/media/blo.bb456ca9.jpeg"
+  },
+  {
+    "revision": "0c2dca1c98f14000cc325ef1fbd4068e",
+    "url": "/makval/static/media/pho.0c2dca1c.png"
+  },
+  {
+    "revision": "f1a1e4da77714255ed45efbc1519f273",
+    "url": "/makval/index.html"
+  }
+];
